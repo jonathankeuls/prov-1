8c4597093dec309665498d873ba7dc8059186020
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-10-19 15:32:52 +0200

    Uppgift 9

---
+++ 9/script.js
@@ -0,0 +1,11 @@
+function bigNames(myArray) {
+    name1 = 'Jonathan'
+    name2 = 'Johan'
+    name3 = 'Alexander'
+
+    myArray = [name1, name2, name3]
+
+    const myArray2 = [name1.toUpperCase(), name2.toUpperCase(), name3.toUpperCase()]
+
+return myArray2
+}
